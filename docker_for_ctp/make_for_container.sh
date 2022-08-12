69c940b55059df7b87c89e5059734fc1053b9dd6
swi0110 <joykevin@cubrid.com>
2022-08-12 16:30:11 +0900

    shell.conf is divided to shell.conf & ha_shell.conf fix the ha_shell test error

--- docker_for_ctp/make_for_container.sh
+++ docker_for_ctp/make_for_container.sh
@@ -8,10 +8,14 @@ CONTAINER_NAME=docker_for_ctp
 ip_range="172.200.1.0"
 ip_list=("172.200.1.2" "172.200.1.3")
 
+# set for shell_HA test
+sed -i "s/MASTER_SERVER_IP=.*/MASTER_SERVER_IP=${ip_list[0]}/" ctp_config/HA.properties
+sed -i "s/SLAVE_SERVER_IP=.*/SLAVE_SERVER_IP=${ip_list[1]}/" ctp_config/HA.properties
+
 # Remove the previous container & image
 IMAGE_ID=`docker images | grep "${IMAGE_NAME}" | awk '{print $3}'`
-docker rm ${CONTAINER_NAME}_01
-docker rm ${CONTAINER_NAME}_02
+docker rm -f ${CONTAINER_NAME}_01
+docker rm -f ${CONTAINER_NAME}_02
 sleep 5;
 docker rmi ${IMAGE_ID}
 sleep 5;
@@ -25,11 +29,6 @@ docker network create --driver bridge br1 --ip-range=${ip_range}/24 --subnet=${i
 #centos 7
 docker run --privileged -dit --cap-add=SYS_PTRACE --net br1 --ip ${ip_list[0]} -v ${dir}/ctp_config:/home/ctp/ctp_config --name ${CONTAINER_NAME}_01 --hostname ${CONTAINER_NAME}_01 ${IMAGE_NAME}:${VERSION} /sbin/init
 docker run --privileged -dit --cap-add=SYS_PTRACE --net br1 --ip ${ip_list[1]} -v ${dir}/ctp_config:/home/ctp/ctp_config --name ${CONTAINER_NAME}_02 --hostname ${CONTAINER_NAME}_02 ${IMAGE_NAME}:${VERSION} /sbin/init
-
-# set for shell_HA test
-sed -i 's/MASTER_SERVER_IP =.*/MASTER_SERVER_IP = ${ip_list[0]}/' ctp_config/HA.properties
-sed -i 's/SLAVE_SERVER_IP =.*/SLAVE_SERVER_IP = ${ip_list[1]}/' ctp_config/HA.properties
-
 sleep 3;
 
 docker exec ${CONTAINER_NAME}_01 systemctl restart sshd

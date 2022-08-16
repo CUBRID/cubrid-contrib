9b7c6f9352016262b372190c3a16cc07aacb7385
swi0110 <joykevin@cubrid.com>
2022-08-16 10:06:00 +0900

    Changed JAVA_HOME's path

--- docker_for_ctp/ctp_config/ctp_env_export.sh
+++ docker_for_ctp/ctp_config/ctp_env_export.sh
@@ -1,6 +1,6 @@
 # !/bin/bash
 
-echo "export JAVA_HOME=/usr/lib/jvm/java-1.6.0-openjdk-1.6.0.41.x86_64" >> ${HOME}/.bash_profile
+echo "export JAVA_HOME=/usr/lib/jvm/java-1.8.0" >> ${HOME}/.bash_profile
 echo "export CTP_HOME=/home/ctp/CTP" >> ${HOME}/.bash_profile
 echo ". ${HOME}/.cubrid.sh" >> ${HOME}/.bash_profile
 sed -i 's@:$HOME/bin@:$HOME/bin:$HOME/CTP/bin:$HOME/CTP/common/script@' ${HOME}/.bash_profile
